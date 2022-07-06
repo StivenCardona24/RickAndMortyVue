0add476734218a3b7f37f4b17114861f685ea44c
Stiven <stiven.cardona.0924@gmail.com>
2022-07-06 04:30:31 -0500

    Version completa sin diseño

--- js/data.js
+++ js/data.js
@@ -2,8 +2,192 @@ var main = new Vue({
     el: "#main",
     data: {
         characters: [],
+        newCharacters: [],
+        users: [],
+        user: "",
+        option: 1,
+        venta: [
+            {
+              Cant: 500,
+              price: 10.0,
+            },
+            {
+              Cant: 1000,
+              price: 20.0,
+            },
+            {
+              Cant: 1500,
+              price: 30.0,
+            },
+            {
+              Cant: 2000,
+              price: 35.0,
+            },
+          ],
+
+          pago: '',
+          rmp: 0,
+          bid: 0
+
     },
     methods: {
+
+
+        loadRmp(){
+            let x = document.getElementById("option1");
+            let y = document.getElementById("option2");
+            let z = document.getElementById("option3");
+            x.classList.add('active');
+            y.classList.remove('active');
+            z.classList.remove('active');
+            this.option = 1;
+
+        },
+
+        loadBuy(){
+            let x = document.getElementById("option1");
+            let y = document.getElementById("option2");
+            let z = document.getElementById("option3");
+            y.classList.add('active');
+            x.classList.remove('active');
+            z.classList.remove('active');
+            this.option = 2;
+        },
+
+        loadHistory(){
+            let x = document.getElementById("option1");
+            let y = document.getElementById("option2");
+            let z = document.getElementById("option3");
+            z.classList.add('active');
+            y.classList.remove('active');
+            x.classList.remove('active');
+            this.option = 3;
+        },
+
+        getRmp(item){
+            this.rmp = item.Cant;
+        },
+        buyRmp(){
+            if(this.pago == ''){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Selecciona un metodo de pago',
+                    
+                   
+                  });
+
+                  return;
+            }
+            else{
+
+               this.user.rmp += this.rmp;
+               Swal.fire(
+                'Has realizado tu compra correctamente',
+                'Presiona el botón',
+                'success'
+              );
+              let btn = document.getElementById('closeRmp');
+              btn.click();
+              this.updateLocalStorage();
+              
+            }
+        },
+
+        offer(person){
+            let offer = document.getElementById(person.id).value;
+            offer = Number(offer);
+            
+            console.log(offer);
+            if(offer == 0){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Debes ofertar un valor',
+                    
+                   
+                  });
+
+                  return;
+            }
+            
+
+            if(offer > this.user.rmp){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'No tienes la cantidad suficiente de rmp, debes comprar más',
+                    
+                   
+                  });
+
+                  return;
+            }
+            let y = offer - person.rmp
+            if(y < 0){
+                let x = person.rmp ;
+                x = x.toFixed(2);
+                person.rmp = x;
+                person.bit -= 1;
+            }
+            else if( y >= 100 && y <= 500 ){
+                let x = person.rmp * 1.1;
+                x = x.toFixed(2);
+                person.rmp = x;
+                person.bit -= 1;
+            }
+            else if(y >= 500 && y <= 1000 ){
+                let x = person.rmp * 1.2;
+                x = x.toFixed(2);
+                person.rmp = x;
+                person.bit -= 1;
+
+            }
+            else{
+                let x = person.rmp * 1.3;
+                x = x.toFixed(2);
+                person.rmp = x;
+                person.bit -= 1;
+            }
+        },
+
+        buyCharacter(item){
+            if(item.rmp > this.user.rmp){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'No tienes la cantidad suficiente de rmp, debes comprar más',
+                    
+                   
+                  });
+                  this.loadRmp();
+                  return;
+                  
+
+            }
+            else{
+                Swal.fire({
+                    title: `¿Estas seguro de comprar a ${item.name}?`,
+                    showDenyButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Save',
+                    denyButtonText: `Don't save`,
+                  }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                      this.user.characters.push(item);
+                      item.ward = false;
+                      Swal.fire(`Felicitaciones! Has comprado a ${item.name}`, '', 'success')
+                      this.user.rmp -= item.rmp;
+                      this.updateLocalStorage();
+                     
+                    } else if (result.isDenied) {
+                      Swal.fire('Compra otros personajes', '', 'info')
+                    }
+                  })
+            }
+
+        },
 
         async listCharacters(){
             
@@ -42,18 +226,19 @@ var main = new Vue({
                     p.rmp = Math.floor(Math.random()* (1500 - 500) + 500);
                     p.bit = Math.floor(Math.random()* (2 - 1) + 1);
                 }
+                p.ward = true;
                 
             });
-      
-            this.updateLocalStorage();
-             
-              
-              
+
+            this.newCharacters = this.characters.filter(u => u.ward == true) ;
+            this.updateLocalStorage();  
 
           },
 
           updateLocalStorage(){
             localStorage.setItem('characters', JSON.stringify(this.characters));
+            localStorage.setItem('users', JSON.stringify(this.users));
+            this.newCharacters = this.characters.filter(u => u.ward == true) ;
             
         
           },
@@ -70,6 +255,37 @@ var main = new Vue({
     }
     else{
         this.listCharacters();
+    }
+
+    if(localStorage.getItem('users') != null){
+        this.users = JSON.parse(localStorage.getItem('users'))
+    }
+
+    if(localStorage.getItem('user') != null){
+        this.user =  JSON.parse(localStorage.getItem('user'));
+
+        this.users.forEach(element => {
+          if(element.id == this.user.id){
+            this.user = element;
+            console.log("hola");
+            this.newCharacters = this.characters.filter(u => u.ward == true) ;
+          }
+        
+
+          
+        });    
+    }
+    else{
+
+        
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'No has iniciado sesión',
+       
+      });
+
+        window.location.href = "../Login.html";
     }
   },
   });
--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ var app = new Vue({
   el: "#app",
   data: {
     users: [
-      {id: 1, name: 'test1', username: 'user1', PIN: '1234', rmp: 1000, characters:[]},
-      {id: 2, name: 'test2', username: 'user2', PIN: '1234', rmp: 3000, Characters:[]},
-      {id: 3, name: 'test3', username: 'user3', PIN: '1234', rmp: 5000, Characters:[]},
+      {id: 1, name: 'test1', username: 'user1', pin: '1234', rmp: 1000, characters:[]},
+      {id: 2, name: 'test2', username: 'user2', pin: '1234', rmp: 3000, Characters:[]},
+      {id: 3, name: 'test3', username: 'user3', pin: '1234', rmp: 5000, Characters:[]},
   ],
   
   option: 1,
   newArrUsers: [],
   username:"",
-  PIN:"",
+  pin:"",
   user:"",
+
   
   person:{
     name:"",
-
+    username: "",
+    pin: '', 
+    pin2: '', 
+    rmp: 0,
   },
 },
 
@@ -25,20 +29,67 @@ var app = new Vue({
   methods: {
     login(){
       if(this.username == "" || this.PIN == ""){ 
-        alert("Ingresa los datos");
-
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Ingresa correctamente los datos del formulario',
+          
+         
+        });
+        this.email = '';
+          this.password = '';
         return
       }else{
         this.users.forEach(persona => {   // recorrer todos los objetos
            if(this.username == persona.username && this.PIN == persona.PIN){
-              alert("¡Bienvenido!");
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+              }
+            })
+            
+            Toast.fire({
+              icon: 'success',
+              title: 'Has iniciado sesión correctamente'
+            })
+  
               this.user = persona;
               this.saveUser();
-              window.location.href="../Web/Index.html" //direccionamiento
+              //direccionamiento
             }
         });
+
+        this.email = '';
+        this.password = '';
         if(this.user==""){
           alert("Los datos son incorrectos");
+        }
+        if(this.user == null){
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'El email y/o contraseña son incorrectos',
+           
+          });
+          this.email = '';
+          this.password = '';
+          return
+        }
+        else{
+          this.email = '';
+          this.password = '';
+          setTimeout(function(){     
+            window.location.href = "Web/Index.html";
+      
+          
+        }.bind(this), 3000);
+  
         }
       }
     },
@@ -46,21 +97,80 @@ var app = new Vue({
     openForm(){
       this.option = 2;
     },
+    openLogin(){
+      this.option = 1;
+    },
+
+    registerUser(){
+
+      if(this.person.name == '' ||  this.person.username == '' || this.person.pin == '' || this.person.pin2 == '' ){
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Ingresa correctamente los datos del formulario',
+          
+         
+        });
+        return
+      }
+
+      else if(this.person.pin !=  this.person.pin2  ){
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'El pin debe coincidir con el otro',
+          
+         
+        });
+        return
+
+      }
+      else{
+
+        this.users.push(
+          {
+            id: this.users.length + 1,
+            name: this.person.name,
+            username: this.person.username,
+            pin: this.person.pin,
+            rmp: 0,
+            characters: [],
+          } 
+        );
+
+        Swal.fire(
+          'Te has registrado correctamente',
+          'Presiona el botón e Inicia sesión',
+          'success'
+        );
+
+        this.updateLocalStorage();
+        
+        this.person.name = ''
+        this.person.username = ''
+        this.person.pin = ''
+        this.person.pin2 =''
+        this.option = 1;
+
+      }
+
+
+
+    },
   
   
-updateLocalStorage() {// actualizar
-  localStorage.setItem('users', JSON.stringify(this.users));
-},
-saveUser(){
-  localStorage.setItem('user', JSON.stringify(this.user));
-}
+  updateLocalStorage() {// actualizar
+    localStorage.setItem('users', JSON.stringify(this.users));
+  },
+  saveUser(){
+    localStorage.setItem('user', JSON.stringify(this.user));
+  }
+
 },
 
 created() {
   if (localStorage.getItem('users') !== null) {
       this.users = JSON.parse(localStorage.getItem('users'))
-  } else {
-      this.login();
   }
  
 },
