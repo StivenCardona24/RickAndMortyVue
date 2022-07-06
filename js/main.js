edf51abde92301c2f776516eb1ad8526d6c35ccb
Stiven <stiven.cardona.0924@gmail.com>
2022-07-05 20:23:27 -0500

    inicio de sesion sirviendo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,69 @@
 var app = new Vue({
   el: "#app",
-  data: {},
-  methods: {},
+  data: {
+    users: [
+      {id: 1, name: 'test1', username: 'user1', PIN: '1234', rmp: 1000, characters:[]},
+      {id: 2, name: 'test2', username: 'user2', PIN: '1234', rmp: 3000, Characters:[]},
+      {id: 3, name: 'test3', username: 'user3', PIN: '1234', rmp: 5000, Characters:[]},
+  ],
+  
+  option: 1,
+  newArrUsers: [],
+  username:"",
+  PIN:"",
+  user:"",
+  
+  person:{
+    name:"",
+
+  },
+},
+
+
+
+
+  methods: {
+    login(){
+      if(this.username == "" || this.PIN == ""){ 
+        alert("Ingresa los datos");
+
+        return
+      }else{
+        this.users.forEach(persona => {   // recorrer todos los objetos
+           if(this.username == persona.username && this.PIN == persona.PIN){
+              alert("¡Bienvenido!");
+              this.user = persona;
+              this.saveUser();
+              window.location.href="../Web/Index.html" //direccionamiento
+            }
+        });
+        if(this.user==""){
+          alert("Los datos son incorrectos");
+        }
+      }
+    },
+
+    openForm(){
+      this.option = 2;
+    },
+  
+  
+updateLocalStorage() {// actualizar
+  localStorage.setItem('users', JSON.stringify(this.users));
+},
+saveUser(){
+  localStorage.setItem('user', JSON.stringify(this.user));
+}
+},
+
+created() {
+  if (localStorage.getItem('users') !== null) {
+      this.users = JSON.parse(localStorage.getItem('users'))
+  } else {
+      this.login();
+  }
+ 
+},
 });
 
   //v-for="i in array" es una directiva de bue que permite recorrer un arreglo
